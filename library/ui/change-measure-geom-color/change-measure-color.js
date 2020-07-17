f92cfa4401ab4452b324466535bc89d5cba80a1b
arnaud <arnaud.sig@gmail.com>
2020-07-17 15:29:26 +0400

    Add script to cvhange default measure color

---
+++ library/ui/change-measure-geom-color/change-measure-color.js
@@ -0,0 +1,39 @@
+lizMap.events.on({
+	'uicreated': function(e) {
+		//set here the style parameters you want to change. example here with red color
+		var sketchSymbolizers = {
+			"Point": {
+				pointRadius: 4,
+				graphicName: "square",
+				fillColor: "white",
+				fillOpacity: 1,
+				strokeWidth: 1,
+				strokeOpacity: 1,
+				strokeColor: "#ce1f2d"
+			},
+			"Line": {
+				strokeWidth: 3,
+				strokeOpacity: 1,
+				strokeColor: "#ce1f2d",
+				strokeDashstyle: "dash"
+			},
+			"Polygon": {
+				strokeWidth: 2,
+				strokeOpacity: 1,
+				strokeColor: "#ce1f2d",
+				strokeDashstyle: "dash",
+				fillColor: "white",
+				fillOpacity: 0.3
+			}
+		};
+		var style = new OpenLayers.Style();
+		style.addRules([
+		new OpenLayers.Rule({symbolizer: sketchSymbolizers})
+		]);
+		var styleMap = new OpenLayers.StyleMap({"default": style});			
+		var measureCtrl = lizMap.map.getControlsByClass("OpenLayers.Control.Measure");
+		for(i in measureCtrl){
+			measureCtrl[i].handlerOptions.layerOptions.styleMap = styleMap;
+		}
+	}
+});
